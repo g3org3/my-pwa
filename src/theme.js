fe91699b955d07aa14eadaf6337872e5c3803a7e
george <7jagjag@gmail.com>
2021-12-27 02:44:21 -0600

    ahora si

---
+++ src/theme.js
@@ -0,0 +1,40 @@
+import { createTheme } from "@mui/material";
+
+export const themeOptions = {
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#cbc5ea',
+    },
+    secondary: {
+      main: '#0277bd',
+    },
+    text: {
+      primary: 'rgba(0,0,0,0.87)',
+      secondary: 'rgba(0,0,0,0.54)',
+      hint: 'rgba(255,255,255,0.38)',
+    },
+  },
+  spacing: 18,
+  shape: {
+    borderRadius: 24,
+  },
+  overrides: {
+    MuiAppBar: {
+      colorInherit: {
+        backgroundColor: '#689f38',
+        color: '#fff',
+      },
+    },
+  },
+  props: {
+    MuiButtonBase: {
+      disableRipple: true,
+    },
+    MuiTooltip: {
+      arrow: true,
+    },
+  },
+};
+
+export const theme = createTheme(themeOptions)
