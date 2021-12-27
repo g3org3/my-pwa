645a9d2d0d7fe438b7989965e8a08733f9cadeeb
george <7jagjag@gmail.com>
2021-12-27 14:36:26 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
+import Box from '@mui/material/Box';
 import Navbar from "./Navbar";
 import BottomNavigation from './BottomNavigation';
 
 function App() {
   return (
     <div style={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
-      <Navbar />
-      <div style={{flex: 1, display: 'flex', justifyContent: 'center'}}>
-        content
-      </div>
+      <Navbar title="PWA" />
+      <Box sx={{flex: 1, display:'flex', background: '#eee', alignItems: 'center', 'justifyContent': 'center'}}>
+        no content
+      </Box>
       <BottomNavigation />
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export default function Navbar() {
+export default function Navbar({ title }) {
   return (
-    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar variant="dense">
           <IconButton
@@ -22,11 +20,9 @@ export default function Navbar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            {title}
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-    </Box>
   );
 }
