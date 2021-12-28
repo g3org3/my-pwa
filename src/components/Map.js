e703a18a0a076952dafb07931c03c4fc1c81d17f
george <7jagjag@gmail.com>
2021-12-27 22:28:02 -0600

    update

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,13 @@
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+// import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const Map = ReactMapboxGl({
-  accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
-});
+// const Map = ReactMapboxGl({
+//   accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+// });
 
 const MyMap = () => {
-  const props = { style: 'mapbox://styles/mapbox/streets-v9' };
-  return (
-    <Map {...props} containerStyle={{ height: '50vh' }}>
-      <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-        <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-      </Layer>
-    </Map>
-  );
+  // const props = { style: 'mapbox://styles/mapbox/streets-v9' };
+  return 'hello';
 };
 
 export default MyMap;
