18249613b2dab3577ff0998c4934559b5ed79585
george <7jagjag@gmail.com>
2021-12-29 10:00:15 -0600

    update

--- src/activities.js
+++ src/activities.js
@@ -11,4 +11,52 @@ export const activities = [
     status: 'confirmado',
     emoji: '🏝',
   },
+  {
+    title: 'Año Nuevo',
+    fecha: '2021-12-31T22:00',
+    status: 'tentativo',
+    emoji: '🎊',
+  },
+  {
+    title: 'Relax',
+    fecha: '2022-01-01T12:00',
+    status: 'confirmado',
+    emoji: '😴',
+  },
+  {
+    title: 'Desayuo o Brunch Antigua',
+    fecha: '2022-01-02T10:00',
+    status: 'tentativo',
+    emoji: '☕️',
+  },
+  {
+    title: 'Partido Final Cremas',
+    fecha: '2022-01-02T20:00',
+    status: 'tentativo',
+    emoji: '⚽️',
+  },
+  {
+    title: 'Visita Tio Coco',
+    fecha: '2022-01-03T15:00',
+    status: 'tentativo',
+    emoji: '👨🏻‍⚕️',
+  },
+  {
+    title: 'Almuerzo Tia Lu',
+    fecha: '2022-01-06T13:00',
+    status: 'tentativo',
+    emoji: '🍕',
+  },
+  {
+    title: 'Almuerzo Pancho loco',
+    fecha: '2022-01-08T13:00',
+    status: 'tentativo',
+    emoji: '🍽',
+  },
+  {
+    title: 'Viaje Francia',
+    fecha: '2022-01-09T04:00',
+    status: 'confirmado',
+    emoji: '🛫',
+  },
 ];
