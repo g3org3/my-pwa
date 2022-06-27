af0801ef0344c09d39eccdebad59a4e5ec48d0f1
Jorge Adolfo <7jagjag@gmail.com>
2022-06-27 16:22:43 +0200

    prettier

--- src/components/Add.js
+++ src/components/Add.js
@@ -46,7 +46,11 @@ const Add = () => {
       <h1>Add new activity</h1>
       <TextField placeholder="title" inputRef={refs.title} />
       <TextField placeholder="fecha" inputRef={refs.fecha} />
-      <TextField placeholder="fecha-hora" type="datetime-local" inputRef={refs.fechahora} />
+      <TextField 
+        placeholder="fecha-hora" 
+        type="datetime-local" 
+        inputRef={refs.fechahora} 
+      />
       <TextField placeholder="status" inputRef={refs.status} />
       <TextField placeholder="emoji" inputRef={refs.emoji} />
       <Button onClick={onSubmit} variant="contained">
