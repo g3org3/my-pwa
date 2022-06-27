69b64f2c2c7d9c7b2e4aae835332f41993a6fb43
Jorge Adolfo <7jagjag@gmail.com>
2022-06-27 17:11:36 +0200

    prettier

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -151,7 +151,7 @@ export default function AlignItemsList() {
                       >
                         {formatDate(fecha)}
                       </Typography>
-                      { lugar && ` — ${lugar}`}
+                      { lugar ? ` — ${lugar}` : null}
                       {` — ${status}`}
                     </React.Fragment>
                   }
